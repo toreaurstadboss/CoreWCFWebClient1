5ab9caf111ee0405995a8f2f8810bf9d449add82
Tore Aurstad <tore.aurstad@gmail.com>
2023-11-22 18:52:31 +0100

    Init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreWCFWebClient1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
